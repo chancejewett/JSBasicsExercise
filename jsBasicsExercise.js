8a453973129a071eb5cfb615025611236f6fb645
Chance Jewett <chancejewett@BCs-MacBook-Pro-3.local>
2020-10-12 15:14:33 -0500

    completed

---
+++ jsBasicsExercise.js
@@ -0,0 +1,21 @@
+//4.
+console.log(17);
+
+//5.NUMBERs,Strings,Boolanes,NULL OR UNDEFINE
+
+//6.+ , - , / , * , %
+
+//7.
+console.log(7%4);
+
+//8.
+console.log("I’M MARY POPPINS, Y'ALL!");
+
+//9.
+console.log("Spider"+ "-" + "man");
+
+//10.
+console.log("Chance".length + "Jewett".length);
+
+//11.
+console.log('Avengers:Endgame'[15]);
